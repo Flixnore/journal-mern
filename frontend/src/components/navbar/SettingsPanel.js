4ecebb884533fa411873d3eb64d914c302c88984
Flixnore <felix.perez.diener@gmail.com>
2022-08-21 00:37:58 -0400

    Create SettingsPanel component

--- frontend/src/components/navbar/SettingsPanel.js
+++ frontend/src/components/navbar/SettingsPanel.js
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react";
+import { getSettings } from "../../util";
 import "./SettingsPanel.css";
 
 function SettingsPanel() {
+  const [loading, setLoading] = useState(true);
+  const [host, setHost] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [db, setDb] = useState("");
+
+
+  useEffect(() => {
+    getSettings().then((data) => {
+      console.log(data)
+      setHost(data.host);
+      setUser(data.user);
+      setPassword(data.password);
+      setDb(data.database);
+    }).catch((err) => {
+      console.log(err);
+    }).finally(() => {
+      setLoading(false);
+    })
+  } , [loading]);
   return (
     <div className="panel">
       {/* display two radio buttons for theme */}
@@ -12,15 +34,23 @@ function SettingsPanel() {
       </div>
       <br />
       <div className="connectionInputs">
-        <label htmlFor="hostInput">Host</label><br />
-        <input type="text" id="hostInput" />
-        <label htmlFor="userInput">User</label><br />
-        <input type="text" id="userInput" />
-        <label htmlFor="passwordInput">Password</label><br />
-        <input type="text" id="passwordInput" />
-        <label htmlFor="databaseInput">Database</label><br />
-        <input type="text" id="dbInput" />
-        <button className="connect">Connect</button>
+        <label htmlFor="hostInput">Host</label>
+        <br />
+        <input type="text" id="hostInput" value={host} />
+        <label htmlFor="userInput">User</label>
+        <br />
+        <input type="text" id="userInput" value={user} />
+        <label htmlFor="passwordInput">Password</label>
+        <br />
+        <input type="text" id="passwordInput" value={password} />
+        <label htmlFor="databaseInput">Database</label>
+        <br />
+        <input type="text" id="dbInput" value={db} />
+        <br />
+        <br />
+      </div>
+      <div>
+        <button>Test</button> | <button>Set</button>
       </div>
     </div>
   );
